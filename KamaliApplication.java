cbca42088a7c5011ebd6534c7d0a17c78e1cbead
KAMALI L <kamali.l2025aids@sece.ac.in>
2026-09-17 08:43:30 +0530

    Add files via upload

---
+++ KamaliApplication.java
@@ -0,0 +1,13 @@
+package com.kamali.kamali;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KamaliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KamaliApplication.class, args);
+	}
+
+}
---
+++ KamaliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamali.kamali;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KamaliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
